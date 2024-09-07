248230f15d6f0675bf04df65f0aab76ffe878830
Eduardo Copat <ecopat@gmail.com>
2024-09-08 11:55:20 +1200

    Dynamic rendering; retitle

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,6 @@ function App() {
     reset();
   }, [reset]);
 
-  //@TODO: add key to each tr and create it dynamically
-
   const generate = () => {
     weeklyPlan.generate(meals, setMeals);
   };
@@ -35,54 +33,30 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Saturday</td>
-            <td>
-              {"Lock: "}
-              <input
-                type="checkbox"
-                checked={meals.Saturday.Lunch.locked}
-                onChange={() => toggleLock("Saturday", "Lunch")}
-              />
-              {meals.Saturday.Lunch.recipeName}
-            </td>
-            <td>{meals.Saturday.Dinner.recipeName}</td>
-          </tr>
-          <tr>
-            <td>Sunday</td>
-            <td>
-              <input
-                type="string"
-                value={meals.Sunday.Lunch.recipeName}
-              ></input>
-            </td>
-            <td>{meals.Sunday.Dinner.recipeName}</td>
-          </tr>
-          <tr>
-            <td>Monday</td>
-            <td>{meals.Monday.Lunch.recipeName}</td>
-            <td>{meals.Monday.Dinner.recipeName}</td>
-          </tr>
-          <tr>
-            <td>Tuesday</td>
-            <td>{meals.Tuesday.Lunch.recipeName}</td>
-            <td>{meals.Tuesday.Dinner.recipeName}</td>
-          </tr>
-          <tr>
-            <td>Wednesday</td>
-            <td>{meals.Wednesday.Lunch.recipeName}</td>
-            <td>{meals.Wednesday.Dinner.recipeName}</td>
-          </tr>
-          <tr>
-            <td>Thursday</td>
-            <td>{meals.Thursday.Lunch.recipeName}</td>
-            <td>{meals.Thursday.Dinner.recipeName}</td>
-          </tr>
-          <tr>
-            <td>Friday</td>
-            <td>{meals.Friday.Lunch.recipeName}</td>
-            <td>{meals.Friday.Dinner.recipeName}</td>
-          </tr>
+          {Object.keys(meals).map((key) => {
+            const meal = meals[key];
+            return (
+              <tr key={key}>
+                <td>{key}</td>
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={meal.Lunch.locked}
+                    onChange={() => toggleLock(key, "Lunch")}
+                  />
+                  {meal.Lunch.recipeName}
+                </td>
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={meal.Dinner.locked}
+                    onChange={() => toggleLock(key, "Dinner")}
+                  />
+                  {meal.Dinner.recipeName}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </>
